f5896f8268930ac550a7bb7ae305fc4e3b736e44
Simone Paterman <spaterman@MacBook-Air-de-Simone.local>
2020-12-09 19:52:01 +0100

    e

---
+++ evalJS.js
@@ -0,0 +1,24 @@
+
+// SIMONE PATERMAN
+
+    //   $('button').click(function (event) {
+    //     event.preventDefault();
+   
+    
+    
+    
+    $('button').on ('click', function (){
+
+$(this).css({"background-color": "red"});
+
+    });
+
+    $('button').on ('dblclick', function (){
+
+$ (this).css ({"background-color": "green"})
+
+    });
+
+
+
+// });
